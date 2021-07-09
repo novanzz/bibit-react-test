2b4ecbbb0954a935e81086047b6660b6be37b5bf
novan ramadhan <novan.ramadhan@kompas.com>
2021-07-09 13:58:40 +0700

    update bibit test

--- logic test/anagram.js
+++ logic test/anagram.js
@@ -5,11 +5,9 @@ function anagram() {
     for (var i = 0; i < words.length; i++) {
         splitWord = words[i].split("")
         id = ascending(splitWord).join("")
-        // insert objek dengan awal harus [] agar tidak undefined
         reducedObj[id] =[reducedObj[id]]+" "+words[i] ;
     }
 
-    //ambil value nya aja tiap object
     var group = Object.values(reducedObj)
     var result = [];
     for (let item in group) {
@@ -26,7 +24,6 @@ function ascending(array) {
     while (!finish) {
         finish = true;
         for (var x = 1; x < array.length; x++) {
-            //buat flow nya ini fungsi sort
             if (array[x - 1] > array[x]) {
                 finish = false;
                 var first = array[x - 1];
